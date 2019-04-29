af878ea9f0384ee5e7a581a1593257492f4442ef
Moiv <spam@moiv.net>
2019-04-29 21:07:24 +1000

    esi scopes updated in Configuration.sample.php

--- Configuration.sample.php
+++ Configuration.sample.php
@@ -9,7 +9,7 @@ define('UNIQUE_STATE', 'thecorp');		// Change this to a unique string
 define('FS_TOKEN_PATH', EVE_SSO_PATH.'tokens/'); // Filesystem Path for Token storage if using FSTokenStorer class
 
 //define('ESI_SCOPE', 'esi-corporations.read_corporation_membership.v1 esi-corporations.read_blueprints.v1');
-define('ESI_SCOPE', 'publicData esi-corporations.read_corporation_membership.v1 esi-characters.read_corporation_roles.v1 esi-wallet.read_corporation_wallets.v1 esi-corporations.read_divisions.v1 esi-corporations.read_contacts.v1 esi-corporations.read_titles.v1 esi-corporations.read_blueprints.v1 esi-bookmarks.read_corporation_bookmarks.v1 esi-corporations.read_standings.v1 esi-markets.read_corporation_orders.v1 esi-corporations.read_facilities.v1 esi-corporations.read_medals.v1 esi-corporations.read_fw_stats.v1 esi-corporations.read_outposts.v1');
+define('ESI_SCOPE', 'publicData esi-corporations.read_corporation_membership.v1 esi-corporations.read_structures.v1 esi-characters.read_corporation_roles.v1 esi-wallet.read_corporation_wallets.v1 esi-corporations.read_divisions.v1 esi-corporations.read_contacts.v1 esi-corporations.read_titles.v1 esi-corporations.read_blueprints.v1 esi-bookmarks.read_corporation_bookmarks.v1 esi-corporations.read_standings.v1 esi-markets.read_corporation_orders.v1 esi-corporations.read_facilities.v1 esi-corporations.read_medals.v1 esi-corporations.read_fw_stats.v1');
 
 include_once EVE_SSO_PATH.'RequestGenerator.php';
 
